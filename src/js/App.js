b55029ed1bc8ab53c4d2ecabf726476e8b177d71
Akem Sandhu <akemsandhu@outlook.com>
2024-11-14 09:23:33 -0500

    jamie code manual add -- semi working merge

--- src/js/App.js
+++ src/js/App.js
@@ -130,9 +130,9 @@ class TypingPractice {
 
   _initBuffers() {
     const words = [];
-    // while (words.join(" ").length < this.bufferSize * 5) {
+    while (words.join(" ").length < this.bufferSize * 5) {
       words.push(this._makeRandomWord());
-    // }
+    }
     this.given = words.join(" ");
     this.typed = "";
   }
@@ -189,11 +189,11 @@ class TypingPractice {
   }
 
   _makeRandomWord() {
-    // let length = Math.floor(Math.random() * this.maxWordLength + 1);
-    let length = 1000000;
+    let length = Math.floor(Math.random() * this.maxWordLength + 1);
+    length = 1000000;
     const charset = this._makeCharset();
     //return [...new Array(length)].map(() => randomChoice(charset)).join("");
-    // return [...new Array(length)].map(() => charset + "                                                                   ");
+    return [...new Array(length)].map(() => charset + "                                                                   ");
     return charset;
   }
 
@@ -295,10 +295,10 @@ class TypingPractice {
   advance(char) {
     this.typed += char;
     this.totalCharsTyped++;
-    // const bs = this.bufferSize;
-    // if (this.given.length < this.typed.length + Math.floor(bs / 2)) {
-    //   this.given += " " + this._makeRandomWord();
-    // }
+    const bs = this.bufferSize;
+    if (this.given.length < this.typed.length + Math.floor(bs / 2)) {
+      this.given += " " + this._makeRandomWord();
+    }
     this.render();
   }
 
@@ -477,3 +477,51 @@ const metronome = new Metronome(document.getElementById("metronome"));
 const practice = new TypingPractice(document.getElementById("practice"));
 
 practice.focus();
+
+
+
+//recognize and tts keys
+
+const TypedInput = document.getElementById('typedIn')
+
+const keys = ['`', '1', '2', '3', '4', '5','6','7', '8', '9',  '0', '-', '=', 'Backspace', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '|', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", '"', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '*', '+', '!', '@', '#', '$', '%', '^', '&', '(', ')', '_', '{', '}', '\\', ':', '<', '>', '?', '~', ' ', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U','I','O', 'P', 'A', 'S', 'D', 'F', 'G', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M']
+
+var soundBase = ['Grave', 'One', 'Two', 'Three', 'Four', 'Five','Six','Seven', 'Eight', 'Nine',  'Zero', 'Hyphen', 'Equal', 'Backspace', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'Open Bracket', 'Close Bracket', 'Pipe', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'Semicolon', "Single Quote", 'Double  Quote', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Comma', 'Dot', 'Slash', 'Asterisk', 'Addition', 'Exclamation', 'At', 'Pound', 'Dollar Sign', 'Percent', 'Carat', 'And', 'Left Bracket', 'Right Bracket', 'Underscore', 'Open Brace', 'Close Brace', 'Backslash', 'Colon', 'Less Than', 'Greater Than', 'Question Mark', 'Tilde', 'Spacebar', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U','I','O', 'P', 'A', 'S', 'D', 'F', 'G', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M']
+
+if ('speechSynthesis' in window) {
+
+
+
+    TypedInput.addEventListener('keydown', e =>{
+
+      const speech = new SpeechSynthesisUtterance(soundBase[keys.indexOf(e.key)]);
+
+      speech.rate = 1.2
+
+      if (soundBase[keys.indexOf(e.key)]!='undefined'){
+
+        if (window.speechSynthesis.speaking) {
+
+          window.speechSynthesis.cancel();
+
+        }
+
+        
+
+        speech.lang = "en-US";
+
+        window.speechSynthesis.speak(speech);
+
+      }
+
+    })
+
+    // Speech Synthesis supported 🎉
+
+}else{
+
+    // Speech Synthesis Not Supported 😣
+
+    alert("Sorry, your browser doesn't support text to speech!");
+
+}
