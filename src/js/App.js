29d7665aa4885608aab634a8f58038c273886fce
Akem Sandhu <akemsandhu@outlook.com>
2024-11-15 11:20:48 -0500

    jamie code works commit

--- src/js/App.js
+++ src/js/App.js
@@ -1,5 +1,6 @@
 "use strict";
 
+
 const LETTERS_LOWER = "abcdefghijklmnopqrstuvwxyz";
 const TOP_ROW = "qwertyuiop"
 const HOME_ROW = "asdfghjkl;"
@@ -13,11 +14,19 @@ const LESSONS = new Map([
   ["Lesson 0", ["xcghasdjlcgysjlcgsjcygsljdcsdlyfgasdlycgslycgsdycs", " "]],
   ["Lesson 1", ["f j", "ff jj", "fj fj jf jf"]],
   ["Lesson 2", ["d k", "dd kk", "dk dk kd kd"]],
-  ["lesson 1000", ["dad"]]
+  ["Lesson 3", ["s l", "ss ll", "sl sl ls ls"]],
+  ["Lesson 4", ["a ;", "aa ;;", "a; a; ;a ;a"]],
+  ["lesson 1000", ["dad"]],
+  ["lesson 2000", [TOP_ROW]],
+  ["lesson 3000", ["wprds"]]
+ 
 ])
-var lessonNum = 1000;//this will be changed based on which lesson you click on
+var lessonNum = 3000;//this will be changed based on which lesson you click on
 var lessonPhase = 0;//changes as lesson progresses
 var lessonCompleted = false;
+var words = 10;
+var wordList = ["hii", "hello", "hey", "woo"]
+
 
 class TypingPractice {
   constructor(root) {
@@ -30,10 +39,12 @@ class TypingPractice {
       weights: root.querySelector(".weights"),
     };
 
+
     this.bufferSize = 35;
     this.focused = false;
     this.maxWordLength = 9;
     this.totalCharsTyped = getLocal("totalCharsTyped") || 0;
+
 
     this._initWeights();
     this._initEvents();
@@ -41,6 +52,7 @@ class TypingPractice {
     this._initLines();
     this.render();
   }
+
 
   _initWeights() {
     const keys = ["lettersLower", "lettersUpper", "digits", "punctuation"];
@@ -50,11 +62,13 @@ class TypingPractice {
     }, {});
   }
 
+
   _saveWeights() {
     Object.getOwnPropertyNames(this.weights).forEach((k) =>
       setLocal(k, this.weights[k])
     );
   }
+
 
   _initEvents() {
     this.dom.input.addEventListener("focus", () => {
@@ -62,14 +76,17 @@ class TypingPractice {
       this.render();
     });
 
+
     this.dom.input.addEventListener("blur", () => {
       this.focused = false;
       this.render();
     });
 
+
     this._charsetRegExp = new RegExp(
       `^[a-zA-Z0-9 ${escapeSpecialRegExpChars(PUNCTUATION)}]\$`
     );
+
 
     this.dom.input.addEventListener("keydown", (e) => {
       if (e.key === "Backspace") {
@@ -83,7 +100,6 @@ class TypingPractice {
         }
         this.backup();
       } else if(e.keyCode == 32 && lessonCompleted){
-        console.log("me");
         lessonNum++;
         lessonPhase = 0;
         this._resetCells();
@@ -98,6 +114,7 @@ class TypingPractice {
       e.preventDefault();
     });
 
+
     this.dom.weights.querySelectorAll(":scope > div").forEach((div) => {
       const getWeightKey = (child) => {
         let elem = child;
@@ -111,6 +128,7 @@ class TypingPractice {
         return key;
       };
 
+
       div.addEventListener("wheel", (e) => {
         const key = getWeightKey(e.target);
         const v = this.weights[key] - Math.sign(e.deltaY);
@@ -123,6 +141,7 @@ class TypingPractice {
         e.preventDefault();
       });
 
+
       div.querySelector("button.incr").addEventListener("click", (e) => {
         const key = getWeightKey(e.target);
         const v = this.weights[key] + 1;
@@ -133,6 +152,7 @@ class TypingPractice {
           this.render();
         }
       });
+
 
       div.querySelector("button.decr").addEventListener("click", (e) => {
         const key = getWeightKey(e.target);
@@ -147,6 +167,7 @@ class TypingPractice {
     });
   }
 
+
   _initBuffers() {
     const words = [];
     // while (words.join(" ").length < this.bufferSize * 5) {
@@ -156,6 +177,7 @@ class TypingPractice {
     this.typed = "";
   }
 
+
   _initLines() {
     const svg = this.dom.weights.querySelector(".lines");
     const x1 = Math.round(svg.clientWidth / 2) - 0.5;
@@ -163,6 +185,7 @@ class TypingPractice {
     const bg = "#fed";
     const stroke = "#dfcfbf";
     const strokeWidth = 2;
+
 
     const makePath = (x1, y1, x2, y2) => {
       const radius = 12;
@@ -172,6 +195,7 @@ class TypingPractice {
       path.setAttributeNS(null, "fill", "none");
       return path;
     };
+
 
     const makePoint = (x, y) => {
       const radius = 3;
@@ -185,6 +209,7 @@ class TypingPractice {
       return circle;
     };
 
+
     this.dom.weights.querySelectorAll(":scope > div").forEach((d) => {
       const x2 =
         Math.round(d.offsetLeft + d.clientWidth / 2 + strokeWidth / 2) + 0.5;
@@ -192,50 +217,81 @@ class TypingPractice {
       svg.append(makePath(x1, y1, x2, y2), makePoint(x2, y2));
     });
 
+
     svg.append(makePoint(x1, y1));
   }
+
 
   _makeCharset() {
     const s = this.weights;
 
 
-      if(lessonNum >= 1000){
-        return ( 
-        LETTERS_LOWER.repeat(s.lettersLower) +
-        LETTERS_UPPER.repeat(s.lettersUpper) +
-        DIGITS.repeat(s.digits) +
-        PUNCTUATION.repeat(s.punctuation)
-        );
+        if(lessonNum >= 1000){
+        if(lessonNum < 2000){
+          return (
+            LETTERS_LOWER.repeat(s.lettersLower) +
+            LETTERS_UPPER.repeat(s.lettersUpper) +
+            DIGITS.repeat(s.digits) +
+            PUNCTUATION.repeat(s.punctuation)
+            );
+        }else{
+          switch(lessonNum){
+            case 2000: return TOP_ROW;
+            case 2001: return BOTTOM_ROW;
+            case 2002: return HOME_ROW;
+            case 2003: return LEFT_HAND;
+            case 2004: return RIGHT_HAND;
+            case 3000: return wordList
+          }
+
+
+        }
+
+
       }else{
          return (LESSONS.get(`Lesson ${lessonNum}`)[lessonPhase]);
       }
 
+
   }
+
 
   _makeRandomWord() {
     // let length = Math.floor(Math.random() * this.maxWordLength + 1);
     let length = 10;
     const charset = this._makeCharset();
-    
-    // return [...new Array(length)].map(() => charset + " 
-    if(lessonNum >= 1000){
+   
+    // return [...new Array(length)].map(() => charset + "
+    if(lessonNum >= 1000 && lessonNum != 3000){
       let typingString = ""
-      for(let i = 0; i < 10; i++ ){
+      for(let i = 0; i < words; i++ ){
         typingString += [...new Array(length)].map(() => randomChoice(charset)).join("")
-        if(i!=9){
+        if(i!=words-1){
           typingString+=" "
-        } 
+        }
+      }
+      return typingString;
+    }
+    else if (lessonNum == 3000){
+      let typingString = ""
+      for(let i = 0; i < words; i++ ){
+        typingString += wordList[Math.floor(Math.random()*wordList.length)];
+        if(i!=words-1){
+          typingString+=" "
+        }
       }
       return typingString;
     }
     else{
       return charset;
     }                                                                  
-    
+   
   }
+
 
   _resetCells() {
     const bs = this.bufferSize;
+
 
     const reset = (parent) => {
       const cells = parent.querySelectorAll("div");
@@ -254,21 +310,26 @@ class TypingPractice {
       }
     };
 
+
     reset(this.dom.given);
     reset(this.dom.typed);
   }
 
+
   get totalCharsTyped() {
     return this._totalCharsTyped || 0;
   }
+
 
   set totalCharsTyped(v) {
     this._totalCharsTyped = v;
     setLocal("totalCharsTyped", v);
   }
 
+
   _renderPractice() {
     this._resetCells();
+
 
     const bs = this.bufferSize;
     const mid = Math.floor(bs / 2);
@@ -276,15 +337,19 @@ class TypingPractice {
     const given = d < 0 ? this.given.slice(0, bs) : this.given.slice(d, d + bs);
     const typed = d < 0 ? this.typed : this.typed.slice(d);
 
+
     let cellsGiven = this.dom.given.querySelectorAll("div");
     let cellsTyped = this.dom.typed.querySelectorAll("div");
+
 
     // Fill the cells.
     [...given].forEach((c, i) => (cellsGiven[i].innerHTML = c));
     [...typed].forEach((c, i) => (cellsTyped[i].innerHTML = c));
 
+
     // Highlight the current top-row cell.
     cellsGiven[typed.length].classList.add("hl");
+
 
     // Highlight and animate the current bottom-row cell.
     if (this.focused) {
@@ -293,6 +358,7 @@ class TypingPractice {
       cellsTyped = this.dom.typed.querySelectorAll("div");
       cellsTyped[typed.length].classList.add("hl", "animated");
     }
+
 
     // Highlight errors.
     let corrects = 0;
@@ -305,17 +371,23 @@ class TypingPractice {
         corrects++;
       }
       if(lessonNum < 1000){
-      if (corrects == LESSONS.get(`Lesson ${lessonNum}`)[lessonPhase].length) {
-        console.log(lessonPhase);
-        corrects = 0;
-        this.nextPhase();
+        if (corrects == LESSONS.get(`Lesson ${lessonNum}`)[lessonPhase].length) {
+          console.log(lessonPhase);
+          corrects = 0;
+          this.nextPhase();
 
+
+        }
       }
+      else if(lessonNum >= 1000 && cellsTyped.length == cellsGiven.length){
+        //exit practice
       }
     });
 
+
     this.dom.count.innerHTML = this.totalCharsTyped;
   }
+
 
   _renderWeights() {
     Object.getOwnPropertyNames(this.weights).forEach((k) => {
@@ -324,10 +396,12 @@ class TypingPractice {
     });
   }
 
+
   render() {
     this._renderPractice();
     this._renderWeights();
   }
+
 
   advance(char) {
     this.typed += char;
@@ -339,6 +413,7 @@ class TypingPractice {
     this.render();
   }
 
+
   backup() {
     if (this.typed.length > 0) {
       this.typed = this.typed.slice(0, -1);
@@ -347,11 +422,13 @@ class TypingPractice {
     }
   }
 
+
   focus() {
     this.dom.input.focus();
     this.focused = true;
     this.render();
   }
+
 
   blur() {
     this.dom.input.blur();
@@ -366,11 +443,13 @@ class TypingPractice {
       lessonPhase++;
     }
 
+
     this._resetCells();
     this._initBuffers();
     this.render();
   }
 }
+
 
 class Metronome {
   constructor(root) {
@@ -382,12 +461,14 @@ class Metronome {
       btnSlower: root.querySelector(".slower"),
     };
 
+
     const bpm = getLocal("metronomeBPM");
     this.bpm = typeof bpm === "undefined" ? 90 : bpm;
     this._intervalID = null;
     this._initEvents();
     this.render();
   }
+
 
   _initEvents() {
     this.dom.root.addEventListener("wheel", (e) => {
@@ -401,9 +482,11 @@ class Metronome {
     this.dom.btnSlower.addEventListener("click", () => (this.bpm -= 5));
   }
 
+
   get bpm() {
     return this._bpm;
   }
+
 
   set bpm(value) {
     const v = parseInt(value);
@@ -414,9 +497,11 @@ class Metronome {
     }
   }
 
+
   _scheduleTick(time) {
     const osc = this._ac.createOscillator();
     const envelope = this._ac.createGain();
+
 
     osc.frequency.value = 800;
     envelope.gain.value = 1;
@@ -425,13 +510,16 @@ class Metronome {
     osc.connect(envelope);
     envelope.connect(this._ac.destination);
 
+
     osc.start(time);
     osc.stop(time + 0.03);
   }
 
+
   get ticking() {
     return this._intervalID !== null;
   }
+
 
   start() {
     if (this.ticking) {
@@ -448,6 +536,7 @@ class Metronome {
     this.render();
   }
 
+
   stop() {
     if (this.ticking) {
       clearInterval(this._intervalID);
@@ -458,9 +547,11 @@ class Metronome {
     this.render();
   }
 
+
   toggle() {
     return this.ticking ? this.stop() : this.start();
   }
+
 
   render() {
     this.dom.root.className = this.ticking ? "on" : "off";
@@ -468,31 +559,38 @@ class Metronome {
   }
 }
 
+
 //
 // BEGIN utils
 //
+
 
 function randomChoice(collection) {
   const n = Math.floor(Math.random() * collection.length);
   return collection[n];
 }
 
+
 function setLocal(key, value) {
   return window.localStorage.setItem(key, JSON.stringify(value));
 }
+
 
 function getLocal(key) {
   const item = window.localStorage.getItem(key);
   return item === null ? undefined : JSON.parse(item);
 }
 
+
 function escapeSpecialRegExpChars(str) {
   return str.replace(/[-[\]{}()*+!<=:?.\/\\^$|#\s,]/g, "\\$&");
 }
 
+
 function makeSvgElement(tagName) {
   return document.createElementNS("http://www.w3.org/2000/svg", tagName);
 }
+
 
 function makeConnectingPath(x1, y1, x2, y2, radius = 25) {
   const dx = x2 - x1;
@@ -504,6 +602,7 @@ function makeConnectingPath(x1, y1, x2, y2, radius = 25) {
   const r = radius > maxRadius ? maxRadius : radius;
   const b = 0.667;
 
+
   const cmds = [
     `M ${x1} ${y1}`,
     `v ${my - sy * r}`,
@@ -513,16 +612,157 @@ function makeConnectingPath(x1, y1, x2, y2, radius = 25) {
     `v ${my - sy * r}`,
   ];
 
+
   const path = makeSvgElement("path");
   path.setAttributeNS(null, "d", cmds.join(" "));
   return path;
 }
 
+
 //
 // END utils
 //
 
+
 const metronome = new Metronome(document.getElementById("metronome"));
 const practice = new TypingPractice(document.getElementById("practice"));
 
+
 practice.focus();
+
+//recognize and tts keys
+
+const TypedInput = document.getElementById('typedIn')
+
+const keys = ['`', '1', '2', '3', '4', '5','6','7', '8', '9',  '0', '-', '=', 'Backspace', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '|', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", '"', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '*', '+', '!', '@', '#', '$', '%', '^', '&', '(', ')', '_', '{', '}', '\\', ':', '<', '>', '?', '~', ' ', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U','I','O', 'P', 'A', 'S', 'D', 'F', 'G', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M']
+
+var soundBase = ['Grave', 'One', 'Two', 'Three', 'Four', 'Five','Six','Seven', 'Eight', 'Nine',  'Zero', 'Hyphen', 'Equal', 'Backspace', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'Open Bracket', 'Close Bracket', 'Pipe', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'Semicolon', "Single Quote", 'Double  Quote', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Comma', 'Dot', 'Slash', 'Asterisk', 'Addition', 'Exclamation', 'At', 'Pound', 'Dollar Sign', 'Percent', 'Carat', 'And', 'Left Bracket', 'Right Bracket', 'Underscore', 'Open Brace', 'Close Brace', 'Backslash', 'Colon', 'Less Than', 'Greater Than', 'Question Mark', 'Tilde', 'Spacebar', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U','I','O', 'P', 'A', 'S', 'D', 'F', 'G', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M']
+
+if ('speechSynthesis' in window) {
+
+
+
+    TypedInput.addEventListener('keydown', e =>{
+
+      const speech = new SpeechSynthesisUtterance(soundBase[keys.indexOf(e.key)]);
+
+      speech.rate = 1.2
+
+      if (soundBase[keys.indexOf(e.key)]!='undefined'){
+
+        if (window.speechSynthesis.speaking) {
+
+          window.speechSynthesis.cancel();
+
+        }
+
+        
+
+        speech.lang = "en-US";
+
+        window.speechSynthesis.speak(speech);
+
+      }
+
+    })
+
+    // Speech Synthesis supported 🎉
+
+}else{
+
+    // Speech Synthesis Not Supported 😣
+
+    alert("Sorry, your browser doesn't support text to speech!");
+
+
+
+
+
+
+
+//recognize and tts keys
+
+
+
+const TypedInput = document.getElementById('typedIn')
+
+
+
+const keys = ['`', '1', '2', '3', '4', '5','6','7', '8', '9',  '0', '-', '=', 'Backspace', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '|', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", '"', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '*', '+', '!', '@', '#', '$', '%', '^', '&', '(', ')', '_', '{', '}', '\\', ':', '<', '>', '?', '~', ' ', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U','I','O', 'P', 'A', 'S', 'D', 'F', 'G', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M']
+
+
+
+var soundBase = ['Grave', 'One', 'Two', 'Three', 'Four', 'Five','Six','Seven', 'Eight', 'Nine',  'Zero', 'Hyphen', 'Equal', 'Backspace', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'Open Bracket', 'Close Bracket', 'Pipe', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'Semicolon', "Single Quote", 'Double  Quote', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Comma', 'Dot', 'Slash', 'Asterisk', 'Addition', 'Exclamation', 'At', 'Pound', 'Dollar Sign', 'Percent', 'Carat', 'And', 'Left Bracket', 'Right Bracket', 'Underscore', 'Open Brace', 'Close Brace', 'Backslash', 'Colon', 'Less Than', 'Greater Than', 'Question Mark', 'Tilde', 'Spacebar', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U','I','O', 'P', 'A', 'S', 'D', 'F', 'G', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M']
+
+
+
+if ('speechSynthesis' in window) {
+
+
+
+
+
+
+
+    TypedInput.addEventListener('keydown', e =>{
+
+
+
+      const speech = new SpeechSynthesisUtterance(soundBase[keys.indexOf(e.key)]);
+
+
+
+      speech.rate = 1.2
+
+
+
+      if (soundBase[keys.indexOf(e.key)]!='undefined'){
+
+
+
+        if (window.speechSynthesis.speaking) {
+
+
+
+          window.speechSynthesis.cancel();
+
+
+
+        }
+
+
+
+        
+
+
+
+        speech.lang = "en-US";
+
+
+
+        window.speechSynthesis.speak(speech);
+
+
+
+      }
+
+
+
+    })
+
+
+
+    // Speech Synthesis supported 🎉
+
+
+
+}else{
+
+
+
+    // Speech Synthesis Not Supported 😣
+
+
+
+    alert("Sorry, your browser doesn't support text to speech!");
+}}
